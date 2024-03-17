b9e484c494661c507579182e57abdb32fa42f2f3
Aleksey Ivanyukov <aa.ivanyukov@gmail.com>
2024-03-17 18:38:03 +0400

    step 4

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+import readlineSync from "readline-sync";
+import getName from "../src/cli.js";
+import getEven from "../src/even.js";
+
+console.log("welcome to the Brain Games");
+getName();
+getEven();
--- src/even.js
+++ src/even.js
@@ -1,5 +1,14 @@
 import readlineSync from "readline-sync";
 
-const even = readlineSync.keyInYN("Answer ""yes"" if the number is even, otherwise answer ""no"");
+const isEven = () => {
+	console.log(
+		'Answer "yes" if the number is even, otherwise answer "no". '
+	);
+	let randomNumber = Math.floor(Math.random() * 100);
+	const evenNumber = randomNumber % 2 === 0;
+	console.log(`Question: ${randomNumber}`);
+	const getAnswer = readlineSync.question("Your answer: ");
+	//return
+};
 
-export { even };
+export default isEven;
