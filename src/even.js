c211289005221a082f62eec3aeb527cea5a5104e
Aleksey Ivanyukov <aa.ivanyukov@gmail.com>
2024-03-31 15:44:37 +0400

    Step 5

--- src/even.js
+++ src/even.js
@@ -1,59 +1,42 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the brain games');
+const greetings = () => {
+  console.log('Welcome to the brain games');
+};
+
+greetings();
+
 const gamerName = readlineSync.question('May I have your name? ');
+
 console.log(`Hello, ${gamerName}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no". ');
 
-let i = 0;
-do {
-const getRandomNumber = Math.floor(Math.random() * 100);
+const isEvenGame = () => {
+  const roundsCount = 3;
 
-const isEven = () => {
-  const evenNumber = getRandomNumber % 2 === 0;
-  return evenNumber;
+  for (let i = 0; i < roundsCount; i += 1) {
+    const getRandomNumber = Math.floor(Math.random() * 100);
+
+    const isEven = () => getRandomNumber % 2 === 0;
+
+    console.log(`Question: ${getRandomNumber}`);
+    const getAnswer = readlineSync.question('Your answer: ');
+
+    const correctAnswer = () => {
+      let answer = '';
+      const isCorrect = isEven() ? answer = 'yes' : answer = 'no';
+      return isCorrect;
+    };
+
+    if (getAnswer !== correctAnswer()) {
+      console.log(`${getAnswer} is wrong answer ;). Correct answer was ${correctAnswer()}. \nLet's try again, ${gamerName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${gamerName}!`);
 };
 
-const getAnswer = () => {
-  console.log(`Question: ${getRandomNumber}`);
-  readlineSync.question('Your answer: ');
-};
-
-const correctAnswer = () => {
-  let answer = '';
-  const isCorrect = isEven() ? answer = 'yes' : answer = 'no';
-  return isCorrect;
-};
-
-isEven();
-getAnswer();
-
-i += 1;
-} while (i < 3);
-
-
-/*  return (getAnswer === correctAnswer) ? console.log('Correct!') : console.log(`'${getAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${gamerName}!`);
-*/
-
-/*
-isEven() === true ? console.log('Correct!') : console.log(`'${getAnswer()}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${gamerName}!`);
-console.log('Correct!');
-*/
-
-/*
-
-isEven();
-getAnswer();
-console.log(correctAnswer());
-
-*/
-
-
-/*
-for (let i = 0; i < 3; i += 1) {
-  isEven() === true ? console.log('Correct!') : console.log(`'${getAnswer()}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${gamerName}!`);
-}
-console.log(`Congratulations, ${gamerName}!`);
-*/
+isEvenGame();
 
 export { gamerName, /*isEven*/ };
